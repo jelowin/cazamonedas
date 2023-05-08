734140a7d5a6d497c6ac3ee5c8aefdb58bf25336
Jenifer López <jenifer.lopez@adevinta.com>
2023-05-08 21:30:06 +0200

    Delete request param from api/coins

--- app/api/coins/route.js
+++ app/api/coins/route.js
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
-export async function GET(request) {
-  // const { searchParams } = new URL(request.url);
-  // const country = searchParams.get("country");
+export async function GET() {
   const { rows } = await sql`SELECT * FROM coins`;
   // const { rows } = await sql`SELECT * FROM coins WHERE country > ${country};`;
   return NextResponse.json(rows);
