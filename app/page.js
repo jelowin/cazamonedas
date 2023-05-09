62841f73333c0a3f398f26d3f1a7c1f97055af26
Jenifer López <jenifer.lopez@adevinta.com>
2023-05-09 16:09:49 +0200

    Comment fetch

--- app/page.js
+++ app/page.js
@@ -3,28 +3,29 @@ import BaseLayout from "@/BaseLayout";
 import CardGrid, { CardGridSkeleton } from "@/CardGrid";
 import FlagCarousel from "@/FlagCarousel";
 import Hero from "@/Hero";
-import { fetchUrl } from "../utils";
+// import { fetchUrl } from "../utils";
 
-const fetchMap = new Map();
+// const fetchMap = new Map();
 
-function queryClient(name, query) {
-  if (!fetchMap.has(name)) {
-    fetchMap.set(name, query());
-  }
+// function queryClient(name, query) {
+//   if (!fetchMap.has(name)) {
+//     fetchMap.set(name, query());
+//   }
 
-  return fetchMap.get(name);
-}
+//   return fetchMap.get(name);
+// }
+
 export default async function Home() {
-  const { rows } = await queryClient("coins", () =>
-    fetch(`${fetchUrl}/api/coins`).then((res) => res.json())
-  );
+  // const { rows } = await queryClient("coins", () =>
+  //   fetch(`${fetchUrl}/api/coins`).then((res) => res.json())
+  // );
 
   return (
     <BaseLayout>
       <Hero />
       <FlagCarousel />
       <Suspense fallback={<CardGridSkeleton />}>
-        <CardGrid data={rows} />;
+        <CardGrid data={[]} />;
       </Suspense>
     </BaseLayout>
   );
--- components/CardGrid.jsx
+++ components/CardGrid.jsx
@@ -24,39 +24,40 @@ export default async function CardGrid({ data = [] }) {
       minChildWidth="250px"
       spacing="24px"
     >
-      {data?.map((coin) => (
-        <Card key={coin.id} maxW="sm" size="md" variant="elevated">
-          <CardBody>
-            <Center>
-              <Img
-                alt={coin.reason}
-                boxSize="200px"
-                src={coin.image}
-                objectFit="cover"
-              />
-            </Center>
-            <Stack mt="6" spacing="3">
-              <Flex justify="space-between">
-                <Heading size="md">{coin.country}</Heading>
-                <Text as="b" fontSize="lg">
-                  {coin.year}
-                </Text>
-              </Flex>
-              <Text fontSize="md">{coin.reason}</Text>
-            </Stack>
-          </CardBody>
-          <CardFooter>
-            <ButtonGroup spacing="2">
-              <Button variant="solid" colorScheme="blue">
-                La tengo
-              </Button>
-              <Button variant="ghost" colorScheme="blue">
-                La quiero
-              </Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
-      ))}
+      {data?.length &&
+        data.map((coin) => (
+          <Card key={coin.id} maxW="sm" size="md" variant="elevated">
+            <CardBody>
+              <Center>
+                <Img
+                  alt={coin.reason}
+                  boxSize="200px"
+                  src={coin.image}
+                  objectFit="cover"
+                />
+              </Center>
+              <Stack mt="6" spacing="3">
+                <Flex justify="space-between">
+                  <Heading size="md">{coin.country}</Heading>
+                  <Text as="b" fontSize="lg">
+                    {coin.year}
+                  </Text>
+                </Flex>
+                <Text fontSize="md">{coin.reason}</Text>
+              </Stack>
+            </CardBody>
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                <Button variant="solid" colorScheme="blue">
+                  La tengo
+                </Button>
+                <Button variant="ghost" colorScheme="blue">
+                  La quiero
+                </Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+        ))}
     </SimpleGrid>
   );
 }
@@ -64,7 +65,7 @@ export default async function CardGrid({ data = [] }) {
 export function CardGridSkeleton() {
   return (
     <SimpleGrid minChildWidth="250px" spacing="24px">
-      {Array.from(Array(8)).map((index) => (
+      {Array.from(Array(8)).map((_, index) => (
         <Skeleton key={index} height="455px" width="auto" />
       ))}
     </SimpleGrid>
