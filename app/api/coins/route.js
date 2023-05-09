98ce6286ff5c96fd6056bcf04e1ec2fed1683e3e
Jenifer López <jenifer.lopez@adevinta.com>
2023-05-09 17:48:45 +0200

    attempt

--- app/api/coins/route.js
+++ app/api/coins/route.js
@@ -4,5 +4,5 @@ import { sql } from "@vercel/postgres";
 export async function GET() {
   const { rows } = await sql`SELECT * FROM coins ORDER BY year DESC LIMIT 20;`;
   // const { rows } = await sql`SELECT * FROM coins WHERE country > ${country};`;
-  return NextResponse.json({ data: rows });
+  return NextResponse.json({ hello: "Next.js" });
 }
