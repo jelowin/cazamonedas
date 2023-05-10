f688e71406aefec07a2ca00658f46d1090593e85
Jenifer López <jenifer.lopez@adevinta.com>
2023-05-10 16:14:10 +0200

    attempt 4

--- app/page.js
+++ app/page.js
@@ -6,27 +6,25 @@ import Hero from "@/Hero";
 import { fetchUrl } from "../utils";
 
 async function getData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const response = await fetch(`${fetchUrl}/api/coins`);
 
   if (!response.ok) {
     const message = `An error has occured fetching data: ${response.status}`;
     throw new Error(message);
   }
 
-  const data = await response.json();
-  return data;
+  return response.json();
 }
 
 export default async function Home() {
-  const data = await getData();
-  // const { rows } = data;
+  const { rows } = await getData();
 
   return (
     <BaseLayout>
       <Hero />
       <FlagCarousel />
       <Suspense fallback={<CardGridSkeleton />}>
-        <CardGrid data={data} />;
+        <CardGrid data={rows} />;
       </Suspense>
     </BaseLayout>
   );
