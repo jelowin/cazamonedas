3106e814b36984e5553984737578717158a697bc
Jenifer López <jenifer.lopez@adevinta.com>
2023-05-12 16:48:09 +0200

    Delete limit

--- app/api/coins/route.js
+++ app/api/coins/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
 export async function GET() {
-  const { rows } = await sql`SELECT * FROM coins ORDER BY year LIMIT 10;`;
+  const { rows } = await sql`SELECT * FROM coins ORDER BY year;`;
   return NextResponse.json(rows);
 }
