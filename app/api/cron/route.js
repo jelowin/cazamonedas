94216c9b1098dbc979a17b7824ce588e9c335d83
Jenifer López <jenifer.lopez@adevinta.com>
2023-09-21 00:33:35 +0200

    Comment crons

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,162 +1,162 @@
-import { NextResponse } from "next/server";
-import { chromium } from "playwright";
-import { db } from "@vercel/postgres";
+// import { NextResponse } from "next/server";
+// import { chromium } from "playwright";
+// import { db } from "@vercel/postgres";
 
-export async function GET() {
-  let country;
-  let description;
-  let reason;
-  let issueDate;
-  let issueVolum;
-  let image;
-  let year;
+// export async function GET() {
+//   let country;
+//   let description;
+//   let reason;
+//   let issueDate;
+//   let issueVolum;
+//   let image;
+//   let year;
 
-  if (process.env.IS_BUILD) {
-    return;
-  }
+//   if (process.env.IS_BUILD) {
+//     return;
+//   }
 
-  const scrappedData = [];
-  const client = await db.connect();
+//   const scrappedData = [];
+//   const client = await db.connect();
 
-  try {
-    const browser = await chromium.launch({
-      headless: true, // setting this to true will not run the UI
-    });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto(
-      "https://www.ecb.europa.eu/euro/coins/comm/html/index.es.html"
-    );
+//   try {
+//     const browser = await chromium.launch({
+//       headless: true, // setting this to true will not run the UI
+//     });
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
+//     await page.goto(
+//       "https://www.ecb.europa.eu/euro/coins/comm/html/index.es.html"
+//     );
 
-    const links = await page.$$eval("a.box", (links) => {
-      console.log(links);
-      return links
-        .filter((link) => link.href.includes("2023"))
-        .map((link) => link.href);
-    });
+//     const links = await page.$$eval("a.box", (links) => {
+//       console.log(links);
+//       return links
+//         .filter((link) => link.href.includes("2023"))
+//         .map((link) => link.href);
+//     });
 
-    for (const link of links) {
-      console.log(`Navigating to ${link}...`);
-      await page.goto(link, {
-        waitUntil: "domcontentloaded",
-      });
+//     for (const link of links) {
+//       console.log(`Navigating to ${link}...`);
+//       await page.goto(link, {
+//         waitUntil: "domcontentloaded",
+//       });
 
-      const elementHandles = await page.$$(".box", (a) => a.innerHTML);
+//       const elementHandles = await page.$$(".box", (a) => a.innerHTML);
 
-      for (const elementHandle of elementHandles) {
-        try {
-          country = await elementHandle.$eval("h3", (node) => node.textContent);
-        } catch {
-          country = "null";
-        }
+//       for (const elementHandle of elementHandles) {
+//         try {
+//           country = await elementHandle.$eval("h3", (node) => node.textContent);
+//         } catch {
+//           country = "null";
+//         }
 
-        try {
-          reason = await elementHandle.$eval(
-            ".content-box:nth-child(2) p",
-            (node) => node
-          );
-          console.log("REASON----", reason.textContent);
-          if (reason.textContent === "") {
-            reason = node.textContent.split(":")[1];
-          } else {
-            reason = reason.textContent;
-          }
-        } catch {
-          reason = "null";
-        }
+//         try {
+//           reason = await elementHandle.$eval(
+//             ".content-box:nth-child(2) p",
+//             (node) => node
+//           );
+//           console.log("REASON----", reason.textContent);
+//           if (reason.textContent === "") {
+//             reason = node.textContent.split(":")[1];
+//           } else {
+//             reason = reason.textContent;
+//           }
+//         } catch {
+//           reason = "null";
+//         }
 
-        try {
-          description = await elementHandle.$eval(
-            "p:nth-of-type(2)",
-            (node) => node.textContent
-          );
-        } catch {
-          description = "null";
-        }
+//         try {
+//           description = await elementHandle.$eval(
+//             "p:nth-of-type(2)",
+//             (node) => node.textContent
+//           );
+//         } catch {
+//           description = "null";
+//         }
 
-        try {
-          image = await elementHandle.$eval(".coins img", (node) => node.src);
-        } catch {
-          image = await elementHandle.$eval(
-            ".coins .coin-cropper",
-            (node) =>
-              `https://www.ecb.europa.eu${node.attributes["data-image"].textContent}`
-          );
-        }
+//         try {
+//           image = await elementHandle.$eval(".coins img", (node) => node.src);
+//         } catch {
+//           image = await elementHandle.$eval(
+//             ".coins .coin-cropper",
+//             (node) =>
+//               `https://www.ecb.europa.eu${node.attributes["data-image"].textContent}`
+//           );
+//         }
 
-        try {
-          issueVolum = await elementHandle.$eval(
-            "p:nth-of-type(3)",
-            (node) => node.textContent
-          );
+//         try {
+//           issueVolum = await elementHandle.$eval(
+//             "p:nth-of-type(3)",
+//             (node) => node.textContent
+//           );
 
-          if (!issueVolum.includes("monedas")) {
-            issueVolum = await elementHandle.$eval(
-              "p:nth-of-type(4)",
-              (node) => node.textContent
-            );
-          }
-        } catch {
-          issueVolum = "null";
-        }
+//           if (!issueVolum.includes("monedas")) {
+//             issueVolum = await elementHandle.$eval(
+//               "p:nth-of-type(4)",
+//               (node) => node.textContent
+//             );
+//           }
+//         } catch {
+//           issueVolum = "null";
+//         }
 
-        try {
-          issueDate = await elementHandle.$eval(
-            "p:nth-of-type(4)",
-            (node) => node.textContent
-          );
+//         try {
+//           issueDate = await elementHandle.$eval(
+//             "p:nth-of-type(4)",
+//             (node) => node.textContent
+//           );
 
-          if (issueDate.includes("monedas")) {
-            issueDate = await elementHandle.$eval(
-              "p:nth-of-type(5)",
-              (node) => node.textContent
-            );
-          }
-        } catch {
-          issueDate = "null";
-        }
+//           if (issueDate.includes("monedas")) {
+//             issueDate = await elementHandle.$eval(
+//               "p:nth-of-type(5)",
+//               (node) => node.textContent
+//             );
+//           }
+//         } catch {
+//           issueDate = "null";
+//         }
 
-        try {
-          year = link.split("/").pop().split(".").shift().split("_").pop();
-        } catch {
-          year = "null";
-        }
+//         try {
+//           year = link.split("/").pop().split(".").shift().split("_").pop();
+//         } catch {
+//           year = "null";
+//         }
 
-        scrappedData.push({
-          ...(country && { country }),
-          ...(description && {
-            description: description.split(":").pop().trim(),
-          }),
-          ...(image && { image }),
-          ...(issueDate && {
-            issueDate: issueDate.split(":").pop().trim(),
-          }),
-          ...(issueVolum && {
-            issueVolum: issueVolum.split(":").pop().trim(),
-          }),
-          ...(reason && { reason: reason.split(":").pop().trim() }),
-          ...(link.length && {
-            year: link.split("/").pop().split(".").shift().split("_").pop(),
-          }),
-        });
-      }
-    }
-    await context.close();
-    await browser.close();
+//         scrappedData.push({
+//           ...(country && { country }),
+//           ...(description && {
+//             description: description.split(":").pop().trim(),
+//           }),
+//           ...(image && { image }),
+//           ...(issueDate && {
+//             issueDate: issueDate.split(":").pop().trim(),
+//           }),
+//           ...(issueVolum && {
+//             issueVolum: issueVolum.split(":").pop().trim(),
+//           }),
+//           ...(reason && { reason: reason.split(":").pop().trim() }),
+//           ...(link.length && {
+//             year: link.split("/").pop().split(".").shift().split("_").pop(),
+//           }),
+//         });
+//       }
+//     }
+//     await context.close();
+//     await browser.close();
 
-    console.log("Inserting data into DB...");
-    for (const data of scrappedData) {
-      await client.sql`
-        INSERT INTO coins(country, description, image, issueDate, issueVolum, reason, year)
-        VALUES (${data.country}, ${data.description}, ${data.image}, ${data.issueDate}, ${data.issueVolum}, ${data.reason}, ${data.year})
-        ON CONFLICT(reason) DO NOTHING
-      `;
-    }
+//     console.log("Inserting data into DB...");
+//     for (const data of scrappedData) {
+//       await client.sql`
+//         INSERT INTO coins(country, description, image, issueDate, issueVolum, reason, year)
+//         VALUES (${data.country}, ${data.description}, ${data.image}, ${data.issueDate}, ${data.issueVolum}, ${data.reason}, ${data.year})
+//         ON CONFLICT(reason) DO NOTHING
+//       `;
+//     }
 
-    await db.end();
-    console.log("END: Rows created");
-    return NextResponse.json({ message: "Rows created", status: 201 });
-  } catch (e) {
-    return NextResponse.json({ message: "Scrapper error", status: 500 });
-  }
-}
+//     await db.end();
+//     console.log("END: Rows created");
+//     return NextResponse.json({ message: "Rows created", status: 201 });
+//   } catch (e) {
+//     return NextResponse.json({ message: "Scrapper error", status: 500 });
+//   }
+// }
--- app/api/cron2/route.js
+++ app/api/cron2/route.js
@@ -1,140 +1,140 @@
-import { NextResponse } from "next/server";
-import { chromium } from "playwright";
-import { db } from "@vercel/postgres";
+// import { NextResponse } from "next/server";
+// import { chromium } from "playwright";
+// import { db } from "@vercel/postgres";
 
-export async function GET() {
-  let country;
-  let description;
-  let reason;
-  let issueDate;
-  let issueVolum;
-  let image;
-  let year;
+// export async function GET() {
+//   let country;
+//   let description;
+//   let reason;
+//   let issueDate;
+//   let issueVolum;
+//   let image;
+//   let year;
 
-  if (process.env.IS_BUILD) {
-    return;
-  }
+//   if (process.env.IS_BUILD) {
+//     return;
+//   }
 
-  const scrappedData = [];
-  //const client = await db.connect();
+//   const scrappedData = [];
+//   //const client = await db.connect();
 
-  try {
-    const browser = await chromium.launch({
-      headless: true, // setting this to true will not run the UI
-    });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto(
-      "https://coleccionismodemonedas.com/catalogo-de-monedas-de-euro/conmemorativas/por-fecha/"
-    );
+//   try {
+//     const browser = await chromium.launch({
+//       headless: true, // setting this to true will not run the UI
+//     });
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
+//     await page.goto(
+//       "https://coleccionismodemonedas.com/catalogo-de-monedas-de-euro/conmemorativas/por-fecha/"
+//     );
 
-    const links = await page.$$eval("a.tdm-image-holder", (links) => {
-      return links
-        .filter((link) => link.href.includes("2023"))
-        .map((link) => link.href);
-    });
+//     const links = await page.$$eval("a.tdm-image-holder", (links) => {
+//       return links
+//         .filter((link) => link.href.includes("2023"))
+//         .map((link) => link.href);
+//     });
 
-    for (const link of links) {
-      console.log(`Navigating to ${link}...`);
-      await page.goto(link, {
-        waitUntil: "domcontentloaded",
-      });
+//     for (const link of links) {
+//       console.log(`Navigating to ${link}...`);
+//       await page.goto(link, {
+//         waitUntil: "domcontentloaded",
+//       });
 
-      await page.waitForSelector("#tdi_88");
+//       await page.waitForSelector("#tdi_88");
 
-      const elementHandles = await page.$$(
-        ".wpb_wrapper:has(.tdm-col-content-info)",
-        (a) => a.innerHTML
-      );
+//       const elementHandles = await page.$$(
+//         ".wpb_wrapper:has(.tdm-col-content-info)",
+//         (a) => a.innerHTML
+//       );
 
-      for (const elementHandle of elementHandles) {
-        console.log("2------------------");
+//       for (const elementHandle of elementHandles) {
+//         console.log("2------------------");
 
-        let coinLinks = await page.$$("a.tdm-col-content-title-url");
+//         let coinLinks = await page.$$("a.tdm-col-content-title-url");
 
-        for (const coinLink of coinLinks) {
-          console.log("COIN LINKM", coinLink);
-          await page.goto(coinLink, {
-            waitUntil: "domcontentloaded",
-          });
-          await page.waitForSelector(".wpb_wrapper");
+//         for (const coinLink of coinLinks) {
+//           console.log("COIN LINKM", coinLink);
+//           await page.goto(coinLink, {
+//             waitUntil: "domcontentloaded",
+//           });
+//           await page.waitForSelector(".wpb_wrapper");
 
-          // const detailLink = await elementHandle.$eval(
-          //   "a.tdm-col-content-title-url",
-          //   (node) => node.href
-          // );
+//           // const detailLink = await elementHandle.$eval(
+//           //   "a.tdm-col-content-title-url",
+//           //   (node) => node.href
+//           // );
 
-          // const enlaces = await page
-          //   .getByRole("link", { name: "Ver ficha" })
-          //   .innerHTML();
-          // console.log("LINK", enlaces);
-          // for (const li of enlaces) {
-          // }
+//           // const enlaces = await page
+//           //   .getByRole("link", { name: "Ver ficha" })
+//           //   .innerHTML();
+//           // console.log("LINK", enlaces);
+//           // for (const li of enlaces) {
+//           // }
 
-          // GO TO DETAIL
-          // await page.goto(detailLink, {
-          //   waitUntil: "domcontentloaded",
-          // });
+//           // GO TO DETAIL
+//           // await page.goto(detailLink, {
+//           //   waitUntil: "domcontentloaded",
+//           // });
 
-          // const featuresInfo = await page.$$(
-          //   ".tdc-zone",
-          //   (node) => node.innerHTML
-          // );
+//           // const featuresInfo = await page.$$(
+//           //   ".tdc-zone",
+//           //   (node) => node.innerHTML
+//           // );
 
-          // reason = await page.$eval(".motivo", (node) => node.textContent);
+//           // reason = await page.$eval(".motivo", (node) => node.textContent);
 
-          country = await page.$eval(
-            "#tdi_99 .tdm-pricing-feature b:first-child",
-            (node) => node.textContent
-          );
+//           country = await page.$eval(
+//             "#tdi_99 .tdm-pricing-feature b:first-child",
+//             (node) => node.textContent
+//           );
 
-          // year = await page.$eval(
-          //   "#tdi_99 .tdm-pricing-feature b:nth-child(2)",
-          //   (node) => node.textContent
-          // );
+//           // year = await page.$eval(
+//           //   "#tdi_99 .tdm-pricing-feature b:nth-child(2)",
+//           //   (node) => node.textContent
+//           // );
 
-          // issueDate = await page.$eval(
-          //   "#tdi_99 .tdm-pricing-feature:nth-child(3) b:first-child",
-          //   (node) => node.textContent
-          // );
+//           // issueDate = await page.$eval(
+//           //   "#tdi_99 .tdm-pricing-feature:nth-child(3) b:first-child",
+//           //   (node) => node.textContent
+//           // );
 
-          // // ".tdi_118 .tdm-pricing-header .tdm-pricing-price-1",
-          // issueVolum = await page.$eval(
-          //   ".tdi_118 ul.tdm-pricing-features li.tdm-pricing-feature:first-child",
-          //   (node) => node.textContent.split(":")[1].trim()
-          // );
+//           // // ".tdi_118 .tdm-pricing-header .tdm-pricing-price-1",
+//           // issueVolum = await page.$eval(
+//           //   ".tdi_118 ul.tdm-pricing-features li.tdm-pricing-feature:first-child",
+//           //   (node) => node.textContent.split(":")[1].trim()
+//           // );
 
-          // description = await page.$eval("#tdi_253 .tdi_256 .tdm-descr", (node) =>
-          //   node.textContent.replace(/[\n\r]+|[\s]{2,}/g, " ").trim()
-          // );
+//           // description = await page.$eval("#tdi_253 .tdi_256 .tdm-descr", (node) =>
+//           //   node.textContent.replace(/[\n\r]+|[\s]{2,}/g, " ").trim()
+//           // );
 
-          // dificulty = await feature.$eval(
-          //   ".tdi_124 .tdm-pricing-header .tdm-pricing-price-1",
-          //   (node) => node.textContent
-          // );
+//           // dificulty = await feature.$eval(
+//           //   ".tdi_124 .tdm-pricing-header .tdm-pricing-price-1",
+//           //   (node) => node.textContent
+//           // );
 
-          console.log({
-            country,
-            year,
-            issueDate,
-            issueVolum,
-            description,
-            reason,
-          });
+//           console.log({
+//             country,
+//             year,
+//             issueDate,
+//             issueVolum,
+//             description,
+//             reason,
+//           });
 
-          await page.goBack();
-          await page.waitForSelector("#tdi_88");
-          coinLinks = await page.$$("a.tdm-col-content-title-url");
-        }
-        /*********************** */
-      }
-    }
+//           await page.goBack();
+//           await page.waitForSelector("#tdi_88");
+//           coinLinks = await page.$$("a.tdm-col-content-title-url");
+//         }
+//         /*********************** */
+//       }
+//     }
 
-    // await db.end();
-    await browser.close();
-    console.log("END: Rows created");
-    return NextResponse.json({ message: "Rows created", status: 201 });
-  } catch (e) {
-    return NextResponse.json({ message: "Scrapper error", status: 500 });
-  }
-}
+//     // await db.end();
+//     await browser.close();
+//     console.log("END: Rows created");
+//     return NextResponse.json({ message: "Rows created", status: 201 });
+//   } catch (e) {
+//     return NextResponse.json({ message: "Scrapper error", status: 500 });
+//   }
+// }
